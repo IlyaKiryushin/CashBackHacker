7f704a3ae3930b198cd6656cf1180dc03fb7e129
Ilya Kiryushin <ilya.cyr@gmail.com>
2026-04-15 16:11:11 +0300

    TestNG commit

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,64 @@
+package ru.netology.service;
+
+
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void calculateBonusIfAmount0 () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals (actual,expected);
+    }
+
+    @Test
+    public void calculateBonusIfAmount1 () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonusIfAmount999 () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonusIfAmount1001 () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonusIfAmount1000 () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void calculateBonusIfAmount1990 () {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1990;
+        int actual = service.remain(amount);
+        int expected = 10;
+        assertEquals(actual, expected);
+    }
+
+}
